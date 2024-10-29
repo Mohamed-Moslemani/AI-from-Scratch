d50d17c9be76b83a57c210626585ea7730a4381a
Mohamed Moslemani <moslemanomohamed@gmail.com>
2024-10-29 23:40:25 +0200

    gradient function done

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,25 +2,43 @@ import math
 import numpy as np 
 import pandas as pd 
 
-def sigmoid(w,x,b):
+def sigmoid(w, x, b):
+    w = np.array(w)
+    x = np.array(x)
+    
+    return 1 / (1 + np.exp(-(np.dot(x, w) + b)))
 
-    return 1/(1+math.exp^(-(np.dot(w,x)+b)))
-
-def logloss(real_vls,preds):
+def logloss(real_vls, preds):
     y_real = np.array(real_vls)
     y_pred = np.array(preds)
     m = len(y_real)
     loss = -np.sum(y_real * np.log(y_pred) + (1 - y_real) * np.log(1 - y_pred)) / m
     return loss
 
-def logistic_gradient(df,target):
-    nrows,ncols = df.shape
-    x = df.drop(columns=target)
-    y = y[target]
-    w = [0]*(ncols - 1)
+def logistic_gradient(df, target, iters=100, lr=0.001):
+    nrows, ncols = df.shape
+    x = df.drop(columns=target).values
+    y = df[target].values
+    w = np.zeros(ncols - 1)
     b = 0
-    y_preds = sigmoid(w,x,b)
-    log_loss = logloss(y,y_preds)
+    log_loss_arr = []
+
+    for i in range(iters): 
+        y_preds = sigmoid(w, x, b)
+        log_loss = logloss(y, y_preds)
+        log_loss_arr.append(log_loss)
+        for j in range(ncols - 1):
+            w[j] -= lr * np.sum((y_preds - y) * x[:, j])
+        b -= lr * np.sum(y_preds - y)
     
-def logistic_regressor():
-    pass
+    return w, b
+
+# Example DataFrame
+df = {
+    'age': [23, 235, 21],
+    'height': [42, 21, 77],
+    'weight': [42, 11, 53]
+}
+
+df = pd.DataFrame(df)
+print(logistic_gradient(df, 'age'))
