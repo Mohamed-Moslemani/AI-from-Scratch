d68ebc9ea7aa7d8950c60120587f2ea7d1d74c52
mohamed-moslemani <moslemanomohamed@gmail.com>
2024-11-09 22:07:21 +0200

    adding different metrics of distance

--- KNN.py
+++ KNN.py
@@ -1,6 +1,14 @@
 import pandas as pd 
 import numpy as np 
 
+def cosine_similarity(x,pt):
+    return np.dot(x, pt) / (np.linalg.norm(x) * np.linalg.norm(pt))
+
+def euclidean_distance(x,pt):
+    return np.sqrt(np.sum(x-pt)**2,axis= 1)
+
+def manhattanDistance(x,pt):
+    return np.abs(np.sum(x-pt))
 
 def KNN(df: pd.DataFrame,k:int,target: str,test_pt: np.array) -> list: 
 
@@ -8,7 +16,7 @@ def KNN(df: pd.DataFrame,k:int,target: str,test_pt: np.array) -> list:
     x = df.drop(columns=[target]).values 
     y= df[target].values 
     
-    distances = np.sqrt(np.sum((x - test_pt)**2, axis=1))
+    distances = euclidean_distance(x,test_pt)
     k_indices= np.argsort(distances)[:k]
 
     return y[k_indices].tolist()
